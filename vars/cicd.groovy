1d22c84c13d111906836cdf16fd6449654e532bd
Ajay Peraboina <107599008+peraboinaajay@users.noreply.github.com>
2023-11-14 09:42:22 +0530

    Update cicd.groovy

--- vars/cicd.groovy
+++ vars/cicd.groovy
@@ -1,9 +1,9 @@
-def gitcode(file){
+def gitcode("file"){
      git "https://github.com/intelliqittrainings/${file}.git"
 }
 def build(){
      sh "mvn package"
 }
-def deploy(job,ip,contex){
+def deploy("job","ip","contex"){
      sh  "scp /var/lib/jenkins/workspace/${job}/webapp/target/webapp.war ubuntu@${ip}:/var/lib/tomcat9/webapps/${contex}.war"
 }
